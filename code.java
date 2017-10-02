e5f24509d702b9e1b1c6478510de5ca1c204b801
TillJo <johanndeiter.till@gmail.com>
2017-10-02 13:46:59 +0200

    Changed the 3 help stacks through variables

--- code.java
+++ code.java
@@ -5,11 +5,10 @@ import java.util.*;
 public class code
 {
 
-	static Stack <BigInteger> fib;
-	static Stack <BigInteger> h1;
-	static Stack <BigInteger> h2;
-	static Stack <BigInteger> h3;
-	static int x;
+	static Stack <BigInteger> fib; //n-2
+	static BigInteger x; //n-1
+	static BigInteger y; //n
+	static int end;
 	static Scanner sdc;
 	
 	
@@ -17,36 +16,22 @@ public class code
 		
 		sdc = new Scanner(System.in);
 		fib = new Stack<BigInteger>();
-		h1 = new Stack<BigInteger>();
-		h2 = new Stack<BigInteger>();
-		h3 = new Stack<BigInteger>();
-		x = sdc.nextInt();
-		fibuntill(x);
+		end = sdc.nextInt();
+		fibuntill(end);
 	}
 	
 	public static void fibuntill (int end)
 	{
-		int x;
-		x = 0;
-		BigInteger big1;
-		BigInteger big2;
-		BigInteger sum;
+
 		fib.push(BigInteger.valueOf(0));
-		h1.push(BigInteger.valueOf(1));
-		h2.push(BigInteger.valueOf(1));
-		
-		while (end>x)
+		fib.push(BigInteger.valueOf(1));
+		x=BigInteger.valueOf(1);
+		for (int i=0; i<end; i++)
 		{
-			
-			big1 = h1.peek();	
-			big2 = h2.peek();
-			sum = big1.add(big2);
-			h3.push(sum);
-			fib.push(h1.pop());
-			h1.push(h2.pop());
-			h2.push(h3.pop());
 			System.out.println(fib.peek());
-			++x;
+			y = x.add(fib.peek());
+			fib.push(x);
+			x=y;
 		}
 	}
-} 
+}
